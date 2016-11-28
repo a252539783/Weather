eac2fe82ade69b51de033077e62b7b162e271af6
a252539783 <252539783@qq.com>
2016-11-28 17:31:25 +0800

    aaa

--- app/src/main/java/xiyou/mobile/android/weather/MainActivity.java
+++ app/src/main/java/xiyou/mobile/android/weather/MainActivity.java
@@ -1,12 +1,27 @@
 package xiyou.mobile.android.weather;
 
 import android.app.Dialog;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.android.volley.NetworkResponse;
 import com.android.volley.RequestQueue;
@@ -23,155 +38,44 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private ListView citylist;
     private RequestQueue rq;
-    private HashMap<String,HashMap> location1=new HashMap<>();
-    private HashMap<String,ArrayList> location0=new HashMap<>();
+    private SelectDialog sd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        citylist=(ListView)findViewById(R.id.citylist);
-
-
         rq= Volley.newRequestQueue(this);
-        Dialog d=new Dialog(this);
-        d.setContentView(R.layout.select_location);
-        d.getWindow().getAttributes().x=-640;
-        d.show();
-        getLocation2();
+        sd=new SelectDialog(this);
     }
 
-    public void getLocation() {
-        rq.add(new NStringRequest("https://passport.baidu.com/js/sitedata_bas.js", new Response.Listener<String>() {
-            @Override
-            public void onResponse(String s) {
-                JSONArray jsonArray= null;
-                JSONObject child=null;
-                try {
-                    jsonArray = new JSONArray(s.substring(13));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                JSONArray sheng=null,shi=null,c=null;
-                JSONObject qu=null;
-                ArrayList shichild=null;
-                HashMap shengchild=null;
-                boolean zhi=true;
-                for (int i=1;i< jsonArray.length();i++)
-                {
-                    try {
-                        child=jsonArray.getJSONObject(i);                      //第i个省/市
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    try {
-
-                            if (child.getInt("type")==0)
-                            {
-                                zhi=true;
-                                shichild=new ArrayList();
-                                location0.put(child.getString("name"),shichild);
-                                Log.e("shi",child.getString("name"));
-                                shi=child.getJSONArray("sub");
-                                c=shi;
-                            }
-                        else
-                            {
-                                zhi=false;
-                                shengchild=new HashMap();
-                                location1.put(child.getString("name"),shengchild);
-                                sheng=child.getJSONArray("sub");
-                                Log.e("sheng",child.getString("name"));
-                                c=sheng;
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        for (int j=0;j<c.length();j++)
-                    {
-                        if (zhi)
-                        {
-                            try {
-                                shichild.add(shi.getJSONObject(j).getString("name"));
-                                Log.e("qu",shi.getJSONObject(j).getString("name"));
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                            continue;
-                        }
-                        else
-                        {
-                            shichild=new ArrayList();
-                            try {
-                                child=sheng.getJSONObject(j);                        //第j个市
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                            try {
-                                shengchild.put(child.getString("name"),shichild);
-                                Log.e("shi",child.getString("name"));
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                            try {
-                                shi=child.getJSONArray("sub");
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        for (int k=0;k<shi.length();k++)
-                        {
-                            try {
-                                shichild.add(shi.getJSONObject(k).getString("name"));
-                                Log.e("qu",shi.getJSONObject(k).getString("name"));
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                Log.e("error",volleyError.toString());
-            }
-        }));
+    @Override
+    public void onClick(View v) {
+        sd.show();
     }
 
-    public void getLocation2()
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.m,menu);
+        TextView v=(TextView) menu.findItem(R.id.select_loc).getActionView();
+        v.setOnClickListener(this);
+        v.setText("选择地区");
+        sd.init();
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public RequestQueue getRq()
     {
-        rq.add(new JsonArrayRequest("http://files.heweather.com/china-city-list.json", new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray jsonArray) {
-                for (int i=0;i<jsonArray.length();i++)
-                {
-                    JSONObject c=null;
-                    try {
-                        c=jsonArray.getJSONObject(i);
-                    if (location1.containsKey(c.getString("provinceZh")))
-                    {
-                        location1.get(c.getString("provinceZh")).put(c.getString("cityZh"),c.getString("id"));
-                    }else
-                    {
-                        HashMap<String,String> x=new HashMap<String, String>();
-                        x.put(c.getString("cityZh"),c.getString("id"));
-                        location1.put(c.getString("provinceZh"),x);
-                    }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                Log.e("error",volleyError.toString());
-            }
-        }));
+        return rq;
     }
 }
---
+++ app/src/main/java/xiyou/mobile/android/weather/SelectDialog.java
@@ -0,0 +1,202 @@
+package xiyou.mobile.android.weather;
+
+import android.app.Dialog;
+import android.app.WallpaperInfo;
+import android.content.Context;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by user on 2016/11/22.
+ */
+
+public class SelectDialog{
+
+    private MainActivity c;
+    private int barHeight=0;
+    private Dialog select_sheng,select_shi;
+    private HashMap<String,ArrayList<String[]>> location1=new HashMap<>();
+    private HashMap<String,ArrayList> location0=new HashMap<>();
+    private HashMap<Integer,BaseAdapter> allShi=new HashMap();
+    private ArrayList<String> sheng=new ArrayList<>();
+    private Window window_sheng,window_shi;
+    private ListView list_shi;
+    private LayoutInflater li;
+
+    public SelectDialog(MainActivity c){
+        this.c=c;
+        li=c.getLayoutInflater();
+    }
+
+    public void show()
+    {
+        select_sheng.show();
+
+    }
+
+    public void init()
+    {
+        select_sheng=new Dialog(c,R.style.DefaultDialog);
+        select_sheng.setContentView(li.inflate(R.layout.select_location,null,false));
+        select_sheng.setCanceledOnTouchOutside(true);
+        select_shi=new Dialog(c,R.style.DefaultDialog);
+        select_shi.setContentView(li.inflate(R.layout.select_location,null,false));
+        select_shi.setCanceledOnTouchOutside(true);
+        window_sheng=select_sheng.getWindow();
+        window_shi=select_shi.getWindow();
+        window_sheng.getAttributes().width=window_shi.getAttributes().width=c.getWindowManager().getDefaultDisplay().getWidth()/4;
+
+        window_shi.getAttributes().height=window_sheng.getAttributes().height=c.getWindowManager().getDefaultDisplay().getHeight()/3;
+        window_sheng.setGravity(Gravity.RIGHT|Gravity.TOP);
+        window_shi.setGravity(Gravity.TOP);
+        window_shi.getAttributes().x+=window_sheng.getAttributes().width/2;;
+        window_sheng.setWindowAnimations(R.style.anim_1);
+        window_shi.setWindowAnimations(R.style.anim_1);
+        list_shi=(ListView)select_shi.findViewById(R.id.list_loc);
+        ((ListView)select_sheng.findViewById(R.id.list_loc)).setAdapter(new ShengAdapter());
+
+        getLocation2();
+        barHeight=(int)c.obtainStyledAttributes(new int[] { android.R.attr.actionBarSize }).getDimension(0,0);
+        window_sheng.getAttributes().y=barHeight;
+    }
+
+    public void getLocation2()
+    {
+        c.getRq().add(new JsonArrayRequest("http://files.heweather.com/china-city-list.json", new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray jsonArray) {
+                for (int i=0;i<jsonArray.length();i++)
+                {
+                    JSONObject c=null;
+                    try {
+                        c=jsonArray.getJSONObject(i);
+                        Log.e("ee",c.getString("provinceZh"));
+                        if (location1.containsKey(c.getString("provinceZh")))
+                        {
+                            location1.get(c.getString("provinceZh")).add(new String[]{c.getString("cityZh"),c.getString("id")});
+                        }else
+                        {
+                            ArrayList<String[]> x=new ArrayList<String[]>();
+                            sheng.add(c.getString("provinceZh"));
+                            x.add(new String[]{c.getString("cityZh"),c.getString("id")});
+                            location1.put(c.getString("provinceZh"),x);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Log.e("error",volleyError.toString());
+            }
+        }));
+    }
+
+    private class ShengAdapter extends BaseAdapter implements View.OnClickListener
+    {
+
+        @Override
+        public int getCount() {
+            Log.e("count",""+sheng.size());
+            return sheng.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView==null||position!=convertView.getId()) {
+                convertView=li.inflate(R.layout.texx, null, false);
+                convertView.setId(position);
+                TextView t=((TextView)convertView.findViewById(R.id.loc_name));
+                t.setText(sheng.get(position));
+                Log.e("aaa",""+position);
+                //t.setHeight();
+                convertView.setOnClickListener(this);
+            }
+
+            return convertView;
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (!allShi.containsKey(v.getId())){
+                allShi.put(v.getId(),new ShiAdapter(sheng.get(v.getId())));
+            }
+
+            list_shi.setAdapter(allShi.get(v.getId()));
+            window_shi.getAttributes().y=(int)v.getY()+barHeight;
+            select_shi.show();
+
+            Log.e("position",v.getX()+":"+v.getY());
+        }
+    }
+
+    private class ShiAdapter extends BaseAdapter
+    {
+        private String sheng;
+        public ShiAdapter(String sheng)
+        {
+            this.sheng=sheng;
+        }
+
+        @Override
+        public int getCount() {
+            return location1.get(sheng).size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView==null||position!=convertView.getId())
+            {
+                convertView=li.inflate(R.layout.texx,null,false);
+                TextView t=(TextView) convertView.findViewById(R.id.loc_name);
+                t.setText(location1.get(sheng).get(position)[0]);
+            }
+
+            return convertView;
+        }
+    }
+}
