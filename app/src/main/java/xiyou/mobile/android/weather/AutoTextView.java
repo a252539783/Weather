b24aa83cb8135374e685cc19740b4bca92a2751a
a252539783 <252539783@qq.com>
2016-12-05 17:18:09 +0800

    aaa

--- app/src/main/java/xiyou/mobile/android/weather/AutoTextView.java
+++ app/src/main/java/xiyou/mobile/android/weather/AutoTextView.java
@@ -9,16 +9,25 @@ import android.widget.TextView;
  */
 
 public class AutoTextView extends TextView {
+    private boolean relyH=true;
+
     public AutoTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        //relyH=attrs.getAttributeBooleanValue("xy3g","relyH",true);
+        relyH=context.obtainStyledAttributes(attrs,R.styleable.xy3g).getBoolean(R.styleable.xy3g_relyH,true);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        if (h>w/getText().toString().length())
-            setTextSize(w/getText().toString().length()*160/getResources().getDisplayMetrics().densityDpi);
-        else
-        setTextSize(h*160/getResources().getDisplayMetrics().densityDpi);
+        if (relyH) {
+            if (h>w/getText().toString().length())
+                setTextSize(w/getText().toString().length()*160/getResources().getDisplayMetrics().densityDpi);
+            else
+                setTextSize(h*160/getResources().getDisplayMetrics().densityDpi);
+        }else
+        {
+                setTextSize(w/getText().toString().length()*160/getResources().getDisplayMetrics().densityDpi);
+        }
     }
 }
--- app/src/main/java/xiyou/mobile/android/weather/LinesView.java
+++ app/src/main/java/xiyou/mobile/android/weather/LinesView.java
@@ -1,8 +1,14 @@
 package xiyou.mobile.android.weather;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by user on 2016/12/4.
@@ -10,8 +16,66 @@ import android.view.View;
 
 public class LinesView extends View {
 
+    Paint p;
+    private int w,h,unitW;
+    private LinesAdapter lines=null;
 
     public LinesView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        p=new Paint();
+        p.setColor(Color.BLACK);
+    }
+
+    public void setAdapter(LinesAdapter la)
+    {
+        lines=la;
+        unitW=w/lines.get(0).length;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        this.w=w;
+        this.h=h;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    public static class LinesAdapter
+    {
+        private ArrayList<int []> lines;
+
+        public LinesAdapter()
+        {
+            lines=new ArrayList<>();
+        }
+
+        public int[] get(int index) {
+            return lines.get(index);
+        }
+
+        public float[] getW()
+        {
+            float []r=new float[lines.size()];
+            float all=0;
+
+            for (int i=0;i<lines.size();i++)
+            {
+                for (int j=0;j<lines.get(i).length;j++)
+                {
+
+                }
+            }
+            return r;
+        }
+
+        public LinesAdapter addLine(int []src)
+        {
+            lines.add(src);
+            return this;
+        }
     }
 }
